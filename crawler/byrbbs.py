83a376080465881fc17fd1a91eb180c5b31981a2
duduscript <duduscript@outlook.com>
2017-02-18 23:54:50 +0800

    topten

--- crawler/byrbbs.py
+++ crawler/byrbbs.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
-import requests
-import os
-import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import requests
+import os
+import json
 
 class User(object):
     def __init__(self,username,password):
@@ -14,23 +14,39 @@ class User(object):
 
 class Crawler(object):
     def __init__(self):
+        self.USER_AGENTS = (
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100 101 Firefox/22.0',
+            'Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0',
+            ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) '
+            'Chrome/19.0.1084.46 Safari/536.5'),
+            ('Mozilla/5.0 (Windows; Windows NT 6.1) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46''Safari/536.5')
+        )
         self.driver = webdriver.PhantomJS()
-        self.driver.get("https://bbs.byr.cn/login")
+        self.driver.get("https://bbs.byr.cn")
+        self.driver.maximize_window()
         time.sleep(3)
         print('--------get--------')
     def login(self,user):
-        username = self.driver.find_element_by_id('u_login_id')
-        password = self.driver.find_element_by_id('u_login_passwd')
+        username = self.driver.find_element_by_id('id')
+        password = self.driver.find_element_by_id('pwd')
         username.send_keys(user.username)
         password.send_keys(user.password)
-        self.driver.find_element_by_id('u_login_submit').click()
-        time.sleep(3)
+        self.driver.find_element_by_id('b_login').click()
+        time.sleep(5)
+    def get_top_ten(self):
+        soup = BeautifulSoup(self.driver.page_source,'lxml')
+        topten_ul = soup.find('li',id='topten').ul
+        for li in topten_ul.find_all('li'):
+            print(li['title'],li.a['href'])
+        
+        
 
 
 if __name__ == '__main__':
     username = os.environ.get('BYR_USERNAME')
     password = os.environ.get('BYR_PASSWORD')
-    print(username,password)
     user = User(username,password)
     crawler = Crawler()
     crawler.login(user)
+    crawler.get_top_ten()
