a2eff71c7bdeced7bcc467497a940bbf734acd02
duduscript <duduscript@outlook.com>
2017-02-24 00:26:52 +0800

    404 500

--- index.py
+++ index.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField
+
 app = Flask(__name__)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+
+class SearchForm(Form):
+    sentence = StringField()
 
 @app.route('/')
 def index():
@@ -13,6 +19,14 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
